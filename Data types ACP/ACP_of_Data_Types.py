f156208f3cd944dfc07334839b7b39e84474a780
Versa-Proggo <s.noirrit.bd@gmail.com>
2025-05-04 20:29:44 +0600

    String operation

--- Data types ACP/ACP_of_Data_Types.py
+++ Data types ACP/ACP_of_Data_Types.py
@@ -1,4 +1,15 @@
-#I used some of my prior knowledge
+#Concatenation
+var1 = "Versa"
+var2 = "Proggo"
+print(f"The variables together are: {var1 +var2}")
+
+#Indexing a string
+variable_string = "Sthitoproggo Noirrit A.K.A Versa_Proggo"
+print({variable_string [::3]})
+print({variable_string [0::1]})
+print({variable_string [::-9999999999999]})
+
+#Upper Case
 word = input("Enter a word: ")
 word2 = word[0].upper() + word[1:]
 print(f"{word} with the first letter capitalized is {word2}")

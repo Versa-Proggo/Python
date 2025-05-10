92061d1b05a746ef4839e3979f7c5ec1906163ca
Versa-Proggo <s.noirrit.bd@gmail.com>
2025-05-10 19:07:04 +0600

    Conditionals ACP

---
+++ Conditional Sentences/ACP/ACP.py
@@ -0,0 +1,16 @@
+day = str(input (f"Enter a day of the week (in lowercase): "))
+if day == "sunday":
+   print(f"On {day}s I meditate in school.")
+elif day == "monday":
+ print(f"On {day}s I do some art.")
+elif day == "tuesday":
+   Print(f"On {day}s I have ECA and I play cricket or Fottball in school.")
+elif day == "wednessday":
+ print(f"I play guitar on {day}s")
+elif day == "thursday":
+ print(f"I always have to give test in school on {day}s")
+elif day == "friday":
+ print(f"I go to Shishu Academy on {day}s")
+else:
+  print(f"I sleep a lot on {day}s")
+ 

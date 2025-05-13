490ef150d20fec2fb10ce325fdc537e837e53ada
Versa-Proggo <s.noirrit.bd@gmail.com>
2025-05-13 18:47:05 +0600

    Operaters/2

---
+++ Operaters/2/Main.py
@@ -0,0 +1,19 @@
+x = True
+y = False
+z = True
+if (x and(not y)and z):
+   print(f"All 3 of them are True")
+else:
+    print(f"At least 1 of them is True")
+a = 10
+b = -10
+c = 0
+if( (a>c) or (b > c)):
+    print(f"Either a>c or b>c is true")
+else:
+    print(f"Neither of numbers is greater than c")
+if (-12 and "VIP"):
+    print(f"Both -12 and 'VIP' is considered as True")
+if a != b:
+    print(f"{a} and {b} are different")
+          

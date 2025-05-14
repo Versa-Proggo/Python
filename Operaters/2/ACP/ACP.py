1e521e82ccfc5b02b689ceec252d64fa8e97ee4d
Versa-Proggo <s.noirrit.bd@gmail.com>
2025-05-14 20:55:32 +0600

    Operaters 2 acp

---
+++ Operaters/2/ACP/ACP.py
@@ -0,0 +1,7 @@
+word = input("Enter a string: ")
+
+if "a"   or "A"  or "b" or "B" or "c" or "C" or "d" or "D" or "e" or "E" or "f" or "F" or "g" or "G" or "h" or "H" or "i" or "I"or "j" or "J" or "k" or "K" or "l" or "L" or "m" or "M" or "n" or "N" or "o" or "O" or "p" or "P" or "q" or "Q" or "r" or "R" or "s" or "S" or "t" or "T" or "u" or "U" or "v" or "V" or "w" or "W" or "x" or "X" or "y" or "Y" or "z" or "Z" in word:
+    print(f"The string {word} contains at least one letter.")
+else:
+    print(f"The string {word} does not contain any letters.")
+

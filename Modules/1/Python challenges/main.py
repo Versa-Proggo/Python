3cdfe8baf52134ea6b7f9100b448f0bbe094d037
Versa-Proggo <s.noirrit.bd@gmail.com>
2025-05-20 18:43:38 +0600

    TEST

---
+++ Modules/1/Python challenges/main.py
@@ -0,0 +1,46 @@
+# a = 4
+# b = 5
+# x =8 
+# y =2
+# z = 0
+# p= (a+b)*x/y
+# print(p)
+
+# name = "Alex"
+# age =0
+# if name == "Alex" or name != "Proggo" and age >= 2:
+#     print("Hello Welcome!")
+# else:
+#     print("Goodbye")
+# the terminal will print "Hello Welcome"(First Comment)
+
+# num1 = float(input(f"Gimme a number: "))
+# num2 = float(input(f"Gimme another number: "))
+# if num1 % num2 == 0:
+#     print(f"{num1} is divisible by {num2} because {num1 / num2}")
+# else:
+#     print(f"{num1} is not divisible by {num2} because {num1 / num2}")
+
+#?3 cycle ride at 10,20,30 km/h find out who slow than avg speed
+# a =10
+# b = 20
+# c= 30
+# avg = (a + b + c)/3
+# print(f"The avg speed the the Cycles are {avg}")
+# if a < avg:
+#     print(f"Cycle A is slower than avg ")
+# elif b < avg:
+#     print(f"Cycle b is slower than avg ")
+# else:
+#     print(f"Cycle C is slower than avg ")
+
+#the avg of 40 differnt no. = 38.0 later, I  read 36 as 56 find out real avg
+sum = 38 * 40
+r_sum = sum - 20
+r_avg = r_sum / 40
+print(r_sum) 
+print(r_avg)
+
+
+
+

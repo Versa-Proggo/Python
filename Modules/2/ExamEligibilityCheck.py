8c99084058ac0804121e785a4424536a2f10f53a
Versa-Proggo <s.noirrit.bd@gmail.com>
2025-05-24 18:42:18 +0600

    NESTED CONDITIONALS

---
+++ Modules/2/ExamEligibilityCheck.py
@@ -0,0 +1,9 @@
+atp = float(input(f"Enter Your ATeendenceeeeee %: "))
+if atp >= 75:
+    print(f"You can give Exam so suffer...")
+else:
+    pre = input(f'HAve not you been womp  womped in the pre test? \n yes = Y \n No = N')
+    if pre == 'Y':
+        print(f'GO U CAN GIVE EXAM SO SUFFER WOMP WOMP' )
+    else:
+        print(f"You cant give Exam  Womp womp ")
---
+++ Modules/2/RIDE.py
@@ -0,0 +1,14 @@
+choice = input('Enter your ride \n 1.bikey \n 2. CAR')
+if choice == "1":
+    b = input(f"what bike do ya wanna ride?????????????????      \n 1.SUZUKI \n 2.Sco0ter ")
+    if b == "1":
+        print(f"Y ou got a  SUZUKI bike ride.")
+    else:
+        print(f"You got a Scooter ride")
+else:
+    c = input(f'What car do ya want? \n 1.Toyota Supra \n 2.TESLA CYBERTRUCK')
+    if c == '1':
+        print(f"You got a TOYATA SUPRA car ride ")
+    else:
+        print(f"You got a TESLA CYBERTRUCK ride")
+    

06e6e25f24b448527f33b39bc6c47a73b1a2e799
Versa-Proggo <s.noirrit.bd@gmail.com>
2025-05-27 17:37:05 +0600

    ACP

---
+++ Modules/2/Nested conditions/ACP.py
@@ -0,0 +1,5 @@
+age=int(input("Enter your age:"))
+if 10<=age<=20:
+    print(f"You are {age} years old, which is between 10 and 20 u goddy goddy go.")
+else:
+    print(f"You are {age} years old, which is NOT between 10 and 20. Womp Womp Womp Womp")
